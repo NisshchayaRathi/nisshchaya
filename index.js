5d02e564c11c1fe649fafa92407398e95c89668a
Nisshchaya Rathi <nisshchayarathi111@gmail.com>
2025-03-26 01:18:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let sidebar=document.body.querySelector('.sidebar')
+function showmenu(){
+    let sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='flex'
+}
+  function hidemenu(){
+    let sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='none'
+}
